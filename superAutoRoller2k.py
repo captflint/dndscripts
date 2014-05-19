1c3daee24a66cfdddcb762925d4ed53d33806b7f
James Stephenson <c4p7.fl1n7@gmail.com>
2014-05-19 10:07:30 -0400

    mo' zeros and progress ftw

--- superAutoRoller2k.py
+++ superAutoRoller2k.py
@@ -1,8 +1,11 @@
 import random
 heads = 0
 tails = 0
-trials = 1000000
+trials = 1000000000
 totaltrials = trials
+def progress():
+    if trials % 1000000 == 0:
+        print((totaltrials - trials) / totaltrials)
 while trials != 0:
     flip = random.randint(0, 1)
     if flip == 1:
@@ -12,5 +15,6 @@ while trials != 0:
     else:
         print('dafuq?')
     trials -= 1
+    progress()
 print('Heads:', heads / totaltrials)
 print('Tails:', tails / totaltrials)
