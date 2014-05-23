c998c2a6cd1153a5128d0d82bbcabc471b90de09
James Stephenson <c4p7.fl1n7@gmail.com>
2014-05-23 13:56:50 -0400

    Added default roll feature to dicetest.py

--- dicetest.py
+++ dicetest.py
@@ -1,7 +1,12 @@
 from roll import roll
 
+lastcommand = 'd20'
 while 1 == 1:
-    command = input('What do you want to roll? ')
+    command = input('What do you want to roll? (' + lastcommand + ') ')
+    if command == '':
+        command = lastcommand
+    else:
+        lastcommand = command
     results = roll(command)
     for r in results:
         print(r)
